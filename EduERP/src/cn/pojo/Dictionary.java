75ebe1f161e5b20daee11c05748e8143b931f655
Administrator <Administrator@hzf>
2018-01-13 11:03:52 +0800

    123123

---
+++ EduERP/src/cn/pojo/Dictionary.java
@@ -0,0 +1,41 @@
+package cn.pojo;
+
+public class Dictionary {
+	//参数表属性
+	private String dictCode;	//类型ID
+	private String diname;		//类型名称
+	private String parentid;		//类型父ID
+	private String sortIndex;		//排序
+	private String remark;		//备注
+	public String getDictCode() {
+		return dictCode;
+	}
+	public void setDictCode(String dictCode) {
+		this.dictCode = dictCode;
+	}
+	public String getDiname() {
+		return diname;
+	}
+	public void setDiname(String diname) {
+		this.diname = diname;
+	}
+	
+	public String getParentid() {
+		return parentid;
+	}
+	public void setParentid(String parentid) {
+		this.parentid = parentid;
+	}
+	public String getSortIndex() {
+		return sortIndex;
+	}
+	public void setSortIndex(String sortIndex) {
+		this.sortIndex = sortIndex;
+	}
+	public String getRemark() {
+		return remark;
+	}
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+}
