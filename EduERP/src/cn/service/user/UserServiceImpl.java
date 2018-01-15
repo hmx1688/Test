bc05da7ac43372882e79ce3c4ac29300557ad332
hmx1688 <530251936@qq.com>
2018-01-15 08:52:12 +0800

    hmx20180115852

---
+++ EduERP/src/cn/service/user/UserServiceImpl.java
@@ -0,0 +1,16 @@
+package cn.service.user;
+
+import java.util.List;
+
+import cn.pojo.User;
+
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public List<User> login(User user) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
