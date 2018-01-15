e8a38d86308f980d06a98e481a524d5e788f2f2e
asus <asus@YouRuiFa>
2018-01-15 11:14:44 +0800

    尤瑞发2018.1.15 11：14

---
+++ EduERP/src/cn/pojo/Job.java
@@ -0,0 +1,27 @@
+package cn.pojo;
+
+public class Job {
+
+	public int id; //ID
+	public int jid; //作业类型
+	public String chaNumber;  //章节序号
+	public String jname; //作业名称
+	public int getJid() {
+		return jid;
+	}
+	public void setJid(int jid) {
+		this.jid = jid;
+	}
+	public String getChaNumber() {
+		return chaNumber;
+	}
+	public void setChaNumber(String chaNumber) {
+		this.chaNumber = chaNumber;
+	}
+	public String getJname() {
+		return jname;
+	}
+	public void setJname(String jname) {
+		this.jname = jname;
+	}
+}
