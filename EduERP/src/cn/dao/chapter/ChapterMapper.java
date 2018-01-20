568445fdd529a2d0c3f0899956838a1703197a42
asus <asus@YouRuiFa>
2018-01-20 12:02:13 +0800

    尤瑞发2018.1.20 12：01

---
+++ EduERP/src/cn/dao/chapter/ChapterMapper.java
@@ -0,0 +1,12 @@
+package cn.dao.chapter;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import cn.dao.CommonDao;
+import cn.pojo.Chapter;
+
+public interface ChapterMapper extends CommonDao<Chapter> {
+
+}
--- EduERP/src/cn/pojo/Job.java
+++ EduERP/src/cn/pojo/Job.java
@@ -2,14 +2,14 @@ package cn.pojo;
 
 public class Job {
 
-	public int id; //ID
-	public int jid; //作业类型
+	public Integer id; //ID
+	public Integer jid; //作业类型
 	public String chaNumber;  //章节序号
 	public String jname; //作业名称
-	public int getJid() {
+	public Integer getJid() {
 		return jid;
 	}
-	public void setJid(int jid) {
+	public void setJid(Integer jid) {
 		this.jid = jid;
 	}
 	public String getChaNumber() {
